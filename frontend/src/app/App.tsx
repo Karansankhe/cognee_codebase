f48be752cf8d2a93abfd7498e0ca7f008b2f8f7d
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-07-03 20:07:45 +0530

    Log in / sign up page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { DashboardPage } from "../pages/dashboard/DashboardPage";
-import { UploadPage } from "../pages/upload/UploadPage";
-import { GraphPage } from "../pages/graph/GraphPage";
-import { TrendsPage } from "../pages/trends/TrendsPage";
+import { AuthPage } from "../pages/auth/AuthPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<UploadPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/graph" element={<GraphPage />} />
-      <Route path="/trends" element={<TrendsPage />} />
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/login" element={<AuthPage />} />
+      <Route path="/signup" element={<AuthPage />} />
+      <Route path="*" element={<AuthPage />} />
     </Routes>
   );
 }
